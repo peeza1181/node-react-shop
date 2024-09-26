5768a7bc995a3d749a5564ec7c7ddf06c275b887
HelloReact <640101030029@dpu.ac.th>
2024-09-26 18:42:49 +0700

    SetupnPayment for Paypal payment and order list and order detail Api

---
+++ README.md
@@ -0,0 +1,4 @@
+# node-react-shop
+
+create react app with vite
+tailwindcss with the flowbite
--- api/routes/Order.js
+++ api/routes/Order.js
@@ -17,7 +17,7 @@ orderRoute.post(
       totalPrice,
       price,
     } = req.body;
-    console.log(orderItems)
+    console.log(orderItems);
 
     if (orderItems && orderItems.length === 0) {
       res.status(400);
@@ -36,6 +36,61 @@ orderRoute.post(
 
       const createdOrder = await order.save();
       res.status(201).json(createdOrder);
+    }
+  })
+);
+
+//orderpayment route
+orderRoute.put(
+  "/:id/payment",
+  protect,
+  asyncHandler(async (req, res) => {
+    const order = await Order.findById(req.params.id);
+
+    if (order) {
+      order.isPaid = true;
+      order.paidAt = Date.now();
+      order.paymentResult = {
+        id: req.body.id,
+        status: req.body.status,
+        updated_time: req.body.updated_time,
+        email_address: req.body.email_address,
+      };
+      const updatedOrder = await order.save();
+      res.status(200).json(updatedOrder);
+    } else {
+      res.status(404);
+      throw new Error("Order Not Found");
+    }
+  })
+);
+
+//get all the orders
+orderRoute.get(
+  "/",
+  protect,
+  asyncHandler(async (req, res) => {
+    const orders = await Order.find({ user: req.user._id }).sort({ _id: -1 });
+    if (orders) {
+      res.status(200).json(orders);
+    } else {
+      res.status(404);
+      throw new Error("Orders Not Found");
+    }
+  })
+);
+
+//get one order by id
+orderRoute.get(
+  "/:id/",
+  protect,
+  asyncHandler(async (req, res) => {
+    const order = await Order.findById(req.params.id).populate("user", "email");
+    if (order) {
+      res.status(200).json(order);
+    } else {
+      res.status(404);
+      throw new Error("Order Not Found");
     }
   })
 );

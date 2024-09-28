f9900e2979bac27a700f0eac4250fd2fbbde0858
HelloReact <640101030029@dpu.ac.th>
2024-09-28 16:18:53 +0700

    Create React App with Tailwindcss and Flowbite and setup home page

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Layouts } from "../Layouts/Layouts";
+import { Products } from "../Components/Products";
+
+export const Home = () => {
+  return (
+    <Layouts>
+      <Products />
+    </Layouts>
+  );
+};
